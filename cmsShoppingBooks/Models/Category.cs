f2b7f85e53aa87dd2ecb053786c849e985eb786b
AlejandraDWI <5721100004@utrng.edu.mx>
2024-05-21 14:02:41 -0600

    agregue una propiedad de fecha de tipo date a la clase Category.cs

---
+++ cmsShoppingBooks/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace cmsShoppingBooks.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required, MinLength(2, ErrorMessage = "Mínimo 2 caracteres!!!")]
+        [RegularExpression(@"^[a-zA-Z-ñ ]+$", ErrorMessage = "Solo se permiten letras!!!")]
+        public string Name { get; set; }
+        public string Slug { get; set; } //descripcion del nombre de la categoria
+        public int Sorting { get; set; }
+        public date Fecha { get; set; }
+
+    }
+}
